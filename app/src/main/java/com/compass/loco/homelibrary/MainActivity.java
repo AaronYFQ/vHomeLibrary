c4951fa5b2f0305eeafe6bed1b1776ae1dd0a5c3
AaronYFQ <aaron.yu@ericsson.com>
2016-07-25 15:24:33 +0800

    Init Commit to setup the project

--- app/src/main/java/com/compass/loco/homelibrary/MainActivity.java
+++ app/src/main/java/com/compass/loco/homelibrary/MainActivity.java
@@ -57,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
         // Do something in response to button
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        Intent intent = new Intent(this, CreateLibraryActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
 
         String message = editText.getText().toString();
