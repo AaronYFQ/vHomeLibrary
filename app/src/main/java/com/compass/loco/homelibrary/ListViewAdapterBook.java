1ee66517cb000e77d9bd84188cc06238ca545e80
EXIAOQU <xiaoyu.qu@ericsson.com>
2016-07-26 16:47:04 +0800

    refine the implementation of manage library

---
+++ app/src/main/java/com/compass/loco/homelibrary/ListViewAdapterBook.java
@@ -0,0 +1,87 @@
+package com.compass.loco.homelibrary;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by EXIAOQU on 7/26/2016.
+ */
+public class ListViewAdapterBook extends BaseAdapter {
+
+    public ArrayList<HashMap<String, String>> myList;
+
+    Activity myActivity;
+
+    CheckBox myCheckBoxBookName;
+    TextView myTextViewBookAuthor;
+    TextView myTextViewBookPublisher;
+    TextView myTextViewBookIsbn;
+    TextView myTextViewBookDetail;
+
+    public ListViewAdapterBook(Activity activity, ArrayList<HashMap<String, String>> list) {
+        super();
+        this.myActivity = activity;
+        this.myList = list;
+    }
+
+    @Override
+    public int getCount() {
+        return myList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return myList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = myActivity.getLayoutInflater();
+        if(convertView == null)
+        {
+            convertView = inflater.inflate(R.layout.activity_manage_library_book_row, null);
+
+            myCheckBoxBookName = (CheckBox)convertView.findViewById(R.id.checkBoxBookName);
+            myTextViewBookAuthor = (TextView)convertView.findViewById(R.id.textViewBookAuthor);
+            myTextViewBookPublisher = (TextView)convertView.findViewById(R.id.textViewBookPublisher);
+            myTextViewBookIsbn = (TextView)convertView.findViewById(R.id.textViewBookIsbn);
+            myTextViewBookDetail = (TextView)convertView.findViewById(R.id.textViewBookDetail);
+
+            myCheckBoxBookName.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    CheckBox cb = (CheckBox)v;
+                    Toast.makeText(myActivity.getApplicationContext(), cb.getText() + " is " + cb.isChecked(), Toast.LENGTH_SHORT ).show();
+                }
+
+            });
+
+        }
+
+        HashMap<String, String> map =  myList.get(position);
+
+        myCheckBoxBookName.setText(map.get("Name"));
+        myTextViewBookAuthor.setText("作者：" + map.get("Author"));
+        myTextViewBookPublisher.setText("出版社：" + map.get("Publisher"));
+        myTextViewBookIsbn.setText("ISBN号：" + map.get("Isbn"));
+        myTextViewBookDetail.setText("简介：" + map.get("Detail"));
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/compass/loco/homelibrary/ManageLibraryActivity.java
+++ app/src/main/java/com/compass/loco/homelibrary/ManageLibraryActivity.java
@@ -1,36 +1,49 @@
 package com.compass.loco.homelibrary;
 
-import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by EXIAOQU on 7/25/2016.
  */
 public class ManageLibraryActivity extends AppCompatActivity {
-    static final String[] BOOKS = new String[] {
-            "Apple", "Banana", "Blackberry"
-    };
 
     TextView myTextViewLibraryName;
+
     Button myButtonAdd;
     Button myButtonDelete;
     Button myButtonApply;
+
     ListView myListViewBooks;
+
+    private ArrayList<HashMap<String, String>> myArrayList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_library);
+
+        init();
+
+        threadGetLibraryBooks();
+        
+    }
+
+    private void init() {
 
         myTextViewLibraryName = (TextView)findViewById(R.id.textViewLibraryName);
 
@@ -39,22 +52,128 @@ public class ManageLibraryActivity extends AppCompatActivity {
         myButtonApply = (Button)findViewById(R.id.buttonApply);
 
         myListViewBooks = (ListView)findViewById(R.id.listViewBooks);
-
-        myTextViewLibraryName.setText("XXX的小店");
-
         myListViewBooks.setTextFilterEnabled(true);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_manage_library_book_row, BOOKS);
-        myListViewBooks.setAdapter(arrayAdapter);
+        myButtonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add();
+            }
+        });
+
+        myButtonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                delete();
+            }
+        });
+
+        myButtonApply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                apply();
+            }
+        });
 
         myListViewBooks.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
                 // show a toast with the TextView test when clicked
-                Toast.makeText(getApplicationContext(), ((TextView)view).getText(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), Integer.toString(position + 1) + " clicked!", Toast.LENGTH_SHORT).show();
             }
         });
 
     }
 
+    private void threadGetLibraryBooks() {
+
+        new Thread( new Runnable() {
+
+            @Override
+            public void run() {
+
+                getLibraryBooks();
+
+            }
+
+        }).start();
+
+    }
+
+
+    private void getLibraryBooks() {
+
+        try {
+            /*
+            URL uri =  new URL("http://ec2-54-67-32-254.us-west-1.compute.amazonaws.com/1");
+            URLConnection ucon = uri.openConnection();
+
+            InputStream is = ucon.getInputStream();
+
+            BufferedInputStream bis = new BufferedInputStream(is);
+
+            StringBuffer sb = new StringBuffer();
+
+            byte[] bytes = new byte[200];
+            int count;
+            while ((count = bis.read(bytes)) != -1) {
+                sb.append(new String(bytes, 0, count));
+            }
+
+            myTextViewLibraryName.setText(sb.toString());
+            */
+
+            myTextViewLibraryName.setText("Unknown Library");
+
+            myArrayList = new ArrayList<HashMap<String, String>>();
+
+            HashMap<String, String> temp;
+
+            temp = new HashMap<String, String>();
+            temp.put("Name", "Apple Book");
+            temp.put("Author", "AppleAuthor");
+            temp.put("Publisher", "ApplePublisher");
+            temp.put("Isbn", "AppleIsbn");
+            temp.put("Detail", "Apple Detail");
+
+            myArrayList.add(temp);
+
+            temp = new HashMap<String, String>();
+            temp.put("Name", "Banana Book");
+            temp.put("Author", "BananaAuthor");
+            temp.put("Publisher", "BananaPublisher");
+            temp.put("Isbn", "BananaIsbn");
+            temp.put("Detail", "Banana Detail");
+
+            myArrayList.add(temp);
+
+            temp = new HashMap<String, String>();
+            temp.put("Name", "Blackberry Book");
+            temp.put("Author", "BlackberryAuthor");
+            temp.put("Publisher", "BlackberryPublisher");
+            temp.put("Isbn", "BlackberryIsbn");
+            temp.put("Detail", "Blackberry Detail");
+
+            myArrayList.add(temp);
+
+            ListViewAdapterBook myListViewAdapterBook = new ListViewAdapterBook(this, myArrayList);
+            myListViewBooks.setAdapter(myListViewAdapterBook);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void add() {
+        Toast.makeText(getApplicationContext(), "Add...", Toast.LENGTH_SHORT).show();
+    }
+
+    private void delete() {
+        Toast.makeText(getApplicationContext(), "Delete...", Toast.LENGTH_SHORT).show();
+    }
+
+    private void apply() {
+        Toast.makeText(getApplicationContext(), "Apply...", Toast.LENGTH_SHORT).show();
+    }
 }
