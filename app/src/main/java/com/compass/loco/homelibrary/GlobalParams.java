0a67c09c04cfa3000388cc1dc54ef5aa75efee6a
elanywa <lanying.wang@ericsson.com>
2016-08-01 11:03:21 +0800

    update login

---
+++ app/src/main/java/com/compass/loco/homelibrary/GlobalParams.java
@@ -0,0 +1,13 @@
+package com.compass.loco.homelibrary;
+
+import android.content.SharedPreferences;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by elanywa on 7/29/2016.
+ */
+public class GlobalParams {
+   public final static String PREF_NAME = "com.compass.loco.homelibrary";
+}
